d5f22e224294e7ad8be4fb129c988b5588f6eaf8
MysteriousOyster <leifwilliambarton@gmail.com>
2026-04-16 15:06:35 -0600

    QOL updates (nothing much)

--- compressor.py
+++ compressor.py
@@ -18,34 +18,35 @@ HTML_FILE_PATH = r"app.html"
 MINIFIED_HTML_PATH = r"app.min.html"
 # Path to write the compressed file to
 COMPRESSED_HTML_PATH = r"app.html.gz"
-# Path to write the C++ constants to
+# Path to write the C++ constants to. Make blank to not save.
 CAMERA_INDEX_FILE = r"camera_index.h"
 # ID for length definition
 LEN_ID = "index_html_gz_len"
 # ID for compressed array constant
 ARR_ID = "index_html_gz"
 
-def compress():
-    with open(HTML_FILE_PATH, "r", encoding="utf-8") as orig:
+def compress(html_file_path: str, minified_html_path: str, compressed_html_path: str, camera_index_file: str):
+    with open(html_file_path, "r", encoding="utf-8") as orig:
         print("minifying...")
         minified = minify_html.minify(orig.read(), minify_css=True, minify_js=True)
-    with open(MINIFIED_HTML_PATH, "wb+") as f_in:
+    with open(minified_html_path, "wb+") as f_in:
         print("encoding...")
         f_in.write(minified.encode("utf-8"))
         f_in.flush()
         f_in.seek(0)
-        with gzip.open(COMPRESSED_HTML_PATH, "wb") as f_out:
+        with gzip.open(compressed_html_path, "wb") as f_out:
             shutil.copyfileobj(f_in, f_out)
-    with open(COMPRESSED_HTML_PATH, "rb") as compressed:
+    with open(compressed_html_path, "rb") as compressed:
         bytes = compressed.read()
 
-    with open(CAMERA_INDEX_FILE, "w") as camera_index:
-        print("writing...")
-        camera_index.write(f"#define {LEN_ID} {len(bytes)}\n")
-        camera_index.write(f"const unsigned char {ARR_ID}[] = {{")
-        for byte in bytes:
-            camera_index.write(f"0x{byte:02X},")
-        camera_index.write("};")
+    if camera_index_file != "":
+        with open(camera_index_file, "w") as camera_index:
+            print("writing...")
+            camera_index.write(f"#define {LEN_ID} {len(bytes)}\n")
+            camera_index.write(f"const unsigned char {ARR_ID}[] = {{")
+            for byte in bytes:
+                camera_index.write(f"0x{byte:02X},")
+            camera_index.write("};")
 
 if __name__ == "__main__":
-    compress()
+    compress(HTML_FILE_PATH, MINIFIED_HTML_PATH, COMPRESSED_HTML_PATH, CAMERA_INDEX_FILE)
